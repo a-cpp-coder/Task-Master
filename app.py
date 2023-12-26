68154f44ce22bb0d20a378b564fff7955a4d2be5
a_cpp_coder <docdaodoi1@gmail.com>
2023-12-26 21:15:45 +0700

    basic Flask application

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # reference to thisfile
 
 @app.route('/') # you are going to pass in the URL string of your route
 
 def index():    # function for that route
-    return "Hello Huy! - test"   
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
